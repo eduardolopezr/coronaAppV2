a4d5fb8ea078ba407fed25759ef33078d4cbed7d
galijg26 <58190894+galijg26@users.noreply.github.com>
2020-12-08 12:16:12 -0600

    Update App.js

--- App.js
+++ App.js
@@ -2,22 +2,31 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator, HeaderTitle} from '@react-navigation/stack';
 
-
 const Stack = createStackNavigator();
 
+import Login from './screens/Login';
 import SignIn from './screens/SignIn';
-import Main from './screens/Main';
-import Menu from './screens/Menu';
+import CoronAppV2 from './screens/CoronAppV2';
+import Temperatura from './screens/Temperatura';
+import Salidas from './screens/Salidas';
+import Sintomas from './screens/Sintomas';
+import Perfil from './screens/Perfil';
+import Configuracion from './screens/Configuracion';
 
 export default function App() {
+  
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Main" component={Main}/>
+        <Stack.Screen name="Login" component={Login}/>
         <Stack.Screen name="SignIn" component={SignIn}/>
-        <Stack.Screen name="Menu" component={Menu}/>
+        <Stack.Screen name="CoronAppV2" component={CoronAppV2}/>
+        <Stack.Screen name="Temperatura" component={Temperatura}/>
+        <Stack.Screen name="Salidas" component={Salidas}/>
+        <Stack.Screen name="Sintomas" component={Sintomas}/>
+        <Stack.Screen name="Perfil" component={Perfil}/>
+        <Stack.Screen name="Configuracion" component={Configuracion}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
